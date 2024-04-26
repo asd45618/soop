72505e7837ae28e3122fd25752e9467a9c9e0a1f
joyoung-e <joyoung_e@naver.com>
2024-04-26 10:47:13 +0900

    슬라이드수정

--- src/components/home/SliderSection.jsx
+++ src/components/home/SliderSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -12,18 +12,44 @@ import sz2 from "@/assets/image/sz2.jpg";
 const SliderSectionblock = styled.div`
   position: relative;
   padding: 10px 0 100px;
+
   .slick-slide {
     height: 40vw;
     background: 100% top / cover no-repeat;
     text-align: right;
+
     img {
-      width: 88%;
+      width: 89%;
       float: right;
+    }
+  }
+
+  .artistMenu {
+    position: relative;
+    top: -750px;
+    right: -30px;
+    float: left;
+
+    ul {
+      z-index: 99999;
+
+      li {
+        margin-bottom: 10px;
+        font-size: 20px;
+        color: #bbb;
+      }
+
+      li.active {
+        color: #000;
+        font-weight: 600;
+      }
     }
   }
 `;
 
 const SliderSection = () => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
   const options = {
     autoplay: true,
     autoplaySpeed: 2000,
@@ -31,6 +57,9 @@ const SliderSection = () => {
     slidesToScroll: 1,
     fade: true,
     dots: true,
+    beforeChange: (current, next) => {
+      setActiveSlideIndex(next);
+    },
   };
 
   return (
@@ -52,6 +81,15 @@ const SliderSection = () => {
           <img src={sz2} alt="slide5" />
         </div>
       </Slider>
+      <div className="artistMenu">
+        <ul>
+          <li className={activeSlideIndex === 0 ? "active" : ""}>GONG YOO</li>
+          <li className={activeSlideIndex === 1 ? "active" : ""}>KIM JAE-UCK</li>
+          <li className={activeSlideIndex === 2 ? "active" : ""}>NAM JOO-HYUK</li>
+          <li className={activeSlideIndex === 3 ? "active" : ""}>SEO HYUN-JIN</li>
+          <li className={activeSlideIndex === 4 ? "active" : ""}>SUZY</li>
+        </ul>
+      </div>
     </SliderSectionblock>
   );
 };
