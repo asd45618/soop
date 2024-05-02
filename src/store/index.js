dcd9425eabbecfcc4e0d2afdf73c5275f1386d9a
kwang99 <88648034+asd45618@users.noreply.github.com>
2024-05-02 09:40:43 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import productReducer from './product'
-
+import { configureStore } from "@reduxjs/toolkit";
+import artistReducer from "./artist";
+import productReducer from "./product";
+import communityReducer from "./community";
+import memberReducer from "./member";
 
 const store = configureStore({
-    reducer : {
-        products : productReducer,
-     
-    }
-})
+  reducer: {
+    artists: artistReducer,
+    products: productReducer,
+    communitys: communityReducer,
+    members: memberReducer,
+  },
+});
 
-export default store; 
+export default store;
