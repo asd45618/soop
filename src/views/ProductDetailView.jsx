2bae40d79ec266d4821b88ed52da24d744013ed6
kwang99 <asd45618@naver.com>
2024-05-09 10:54:41 +0900

    product

--- src/views/ProductDetailView.jsx
+++ src/views/ProductDetailView.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'
-import Title from '@/components/layout/Title'
-import ProductDetailSection from '@/components/product/ProductDetailSection'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Title from "@/components/layout/Title";
+import ProductDetailSection from "@/components/product/ProductDetailSection";
 
 const ProductDetailView = () => {
-    const location = useLocation()
-    const { item } = location.state || {};
-    return (
-            <div>
-                <Title title="" />
-                <div className="row">
-                    <ProductDetailSection product={item}/>
-                </div>
-            </div>
-    );
+  const location = useLocation();
+  const { item } = location.state || {};
+  return (
+    <div>
+      <Title title="" />
+      <div className="row">
+        <ProductDetailSection product={item} />
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetailView;
